b02b0475f1dbefb9952807d3f7b13922b9b9a230
Artur Kyung Min Lee <60680481+turtuguita@users.noreply.github.com>
2021-08-18 08:26:40 -0300

    update

--- EXEMPLO1.C
+++ EXEMPLO1.C
@@ -19,9 +19,19 @@ void far processo2(){
     termina_processo();
 }
 
+void far processo3(){
+    int i = 0;
+    while (i < 10000){
+        printf("3");
+        i++;
+    }
+    termina_processo();
+}
+
 
 int main (){
     cria_processo("p1", processo1);
     cria_processo("p2", processo2);
+    cria_processo("p3", processo3);
     dispara_sistema();
 }
